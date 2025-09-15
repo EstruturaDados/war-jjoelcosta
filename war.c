969004a452f07dd9e8b9d2c1b2541bf2029115f7
Joel Lacerda <jjoelcosta@icloud.com>
2025-09-15 03:22:51 +0000

    Teste digitação

--- war.c
+++ war.c
@@ -18,7 +18,6 @@ int main(void) {
         printf("Digite o nome do territorio: ");
         scanf("%29s", territorios[i].nome);
 
-        // Mensagem com instrução e exemplos
         printf("Digite a cor do exercito (ex: Azul, Verde): ");
         scanf("%9s", territorios[i].cor);
 
@@ -26,13 +25,13 @@ int main(void) {
         scanf("%d", &territorios[i].tropas);
     }
 
-    // Exibição dos territórios cadastrados
-    printf("\n=== Territorios Cadastrados ===\n");
+    // Exibição em formato de mapa do mundo
+    printf("\n=== MAPA DO MUNDO ===\n");
     for (int i = 0; i < 5; i++) {
-        printf("\nTerritorio %d\n", i + 1);
-        printf("Nome: %s\n", territorios[i].nome);
-        printf("Cor do exercito: %s\n", territorios[i].cor);
-        printf("Quantidade de tropas: %d\n", territorios[i].tropas);
+        printf("\nTERRITORIO %d:\n", i + 1);
+        printf("- Nome: %s\n", territorios[i].nome);
+        printf("- Dominado por Exercito %s\n", territorios[i].cor);
+        printf("- Tropas: %d\n", territorios[i].tropas);
     }
 
     return 0;
