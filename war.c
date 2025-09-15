9d860e051020372c5b3c043a6f5b4110e6e8886a
Joel Lacerda <jjoelcosta@icloud.com>
2025-09-15 03:02:31 +0000

    exibir os 5 territorios cadastrados

--- war.c
+++ war.c
@@ -50,3 +50,41 @@ int main(void) {
     printf("\nCadastro concluido.\n");
     return 0;
 }
+#include <stdio.h>
+
+// Struct Territorio conforme solicitado
+typedef struct {
+    char nome[30];
+    char cor[10];
+    int tropas;
+} Territorio;
+
+int main(void) {
+    Territorio territorios[5];
+
+    // Cadastro
+    printf("=== Cadastro de Territorios ===\n");
+    for (int i = 0; i < 5; i++) {
+        printf("\nTerritorio %d\n", i + 1);
+
+        printf("Digite o nome do territorio: ");
+        scanf("%29s", territorios[i].nome);
+
+        printf("Digite a cor do exercito: ");
+        scanf("%9s", territorios[i].cor);
+
+        printf("Digite a quantidade de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+    }
+
+    // Exibição
+    printf("\n=== Territorios Cadastrados ===\n");
+    for (int i = 0; i < 5; i++) {
+        printf("\nTerritorio %d\n", i + 1);
+        printf("Nome: %s\n", territorios[i].nome);
+        printf("Cor do exercito: %s\n", territorios[i].cor);
+        printf("Quantidade de tropas: %d\n", territorios[i].tropas);
+    }
+
+    return 0;
+}
